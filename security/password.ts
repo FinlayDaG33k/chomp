d521b9f7da70f2ac6a0b9ba4f55335c6d90729cd
Aroop Roelofs <me@finlaydag33k.nl>
2022-03-05 16:02:41 +0100

    Decouple hashing from password class

--- security/password.ts
+++ security/password.ts
@@ -1,75 +1,49 @@
-import { DigestAlgorithm, crypto } from "../deps.ts";
-import { Random } from "./random.ts";
+import {Random} from "./random.ts";
+import {Algorithms, Hash} from "./hash.ts";
 
 /**
- * List of algorithms supported by this library
+ *  Recommended hashing algorithm for most use-cases.
+ *  May change over time to keep up with NIST approved algorithms
  */
-/* Recommended, may change over time to keep up with NIST approved algorithms */
-export const PASSWORD_DEFAULT = 'SHA3-256';
-export const PASSWORD_SHA384 = 'SHA-384';
-export const PASSWORD_SHA3_224 = 'SHA3-224';
-export const PASSWORD_SHA3_256 = 'SHA3-256';
-export const PASSWORD_SHA3_384 = 'SHA3-384';
-export const PASSWORD_SHA3_512 = 'SHA3-512';
-export const PASSWORD_SHAKE128 = 'SHAKE128';
-export const PASSWORD_SHAKE256 = 'SHAKE256';
-export const PASSWORD_BLAKE2B256 = "BLAKE2B-256";
-export const PASSWORD_BLAKE2B384 = "BLAKE2B-384";
-export const PASSWORD_BLAKE2B = "BLAKE2B";
-export const PASSWORD_BLAKE2S = "BLAKE2S";
-export const PASSWORD_BLAKE3 = "BLAKE3";
-export const PASSWORD_KECCAK224 = "KECCAK-224";
-export const PASSWORD_KECCAK256 = "KECCAK-256";
-export const PASSWORD_KECCAK384 = "KECCAK-384";
-export const PASSWORD_KECCAK512 = "KECCAK-512";
-/* Insecure, please do not use in production */
-export const PASSWORD_SHA224 = 'SHA-224';
-/* Insecure, please do not use in production */
-export const PASSWORD_SHA256 = 'SHA-256';
-/* Insecure, please do not use in production */
-export const PASSWORD_SHA512 = 'SHA-512';
-/* Insecure, please do not use in production */
-export const PASSWORD_SHA1 = 'SHA-1';
-/* Insecure, please do not use in production */
-export const PASSWORD_MD5 = 'MD5'
+export const PASSWORD_DEFAULT = Algorithms.SHA3_256;
 
 /**
  * Create a mapping of algorithms to identifiers
  * Please *never* change the order of these (only append)
  */
 const HASH_IDENTIFIERS = [
-  PASSWORD_MD5,
-  PASSWORD_SHA1,
-  PASSWORD_SHA224,
-  PASSWORD_SHA256,
-  PASSWORD_SHA384,
-  PASSWORD_SHA512,
-  PASSWORD_SHA3_224,
-  PASSWORD_SHA3_256,
-  PASSWORD_SHA3_384,
-  PASSWORD_SHA3_512,
-  PASSWORD_SHAKE128,
-  PASSWORD_SHAKE256,
-  PASSWORD_BLAKE2B256,
-  PASSWORD_BLAKE2B384,
-  PASSWORD_BLAKE2B,
-  PASSWORD_BLAKE2S,
-  PASSWORD_BLAKE3,
-  PASSWORD_KECCAK224,
-  PASSWORD_KECCAK256,
-  PASSWORD_KECCAK384,
-  PASSWORD_KECCAK512
+  Algorithms.MD5,
+  Algorithms.SHA1,
+  Algorithms.SHA224,
+  Algorithms.SHA256,
+  Algorithms.SHA384,
+  Algorithms.SHA512,
+  Algorithms.SHA3_224,
+  Algorithms.SHA3_256,
+  Algorithms.SHA3_384,
+  Algorithms.SHA3_512,
+  Algorithms.SHAKE128,
+  Algorithms.SHAKE256,
+  Algorithms.BLAKE2B256,
+  Algorithms.BLAKE2B384,
+  Algorithms.BLAKE2B,
+  Algorithms.BLAKE2S,
+  Algorithms.BLAKE3,
+  Algorithms.KECCAK224,
+  Algorithms.KECCAK256,
+  Algorithms.KECCAK384,
+  Algorithms.KECCAK512
 ];
 
 /**
  * Specify algorithms that are supported but deemed insecure
  */
 const INSECURE_ALGORITHMS: string[] = [
-  PASSWORD_SHA224,
-  PASSWORD_SHA256,
-  PASSWORD_SHA512,
-  PASSWORD_SHA1,
-  PASSWORD_MD5
+  Algorithms.SHA224,
+  Algorithms.SHA256,
+  Algorithms.SHA512,
+  Algorithms.SHA1,
+  Algorithms.MD5,
 ];
 
 // Set default options for hashing
@@ -97,7 +71,7 @@ export class Password {
    * @param options
    * @returns Promise<string> Hash string containing algo, cost, salt and hash
    */
-  public static async hash(password: string, algo: string = PASSWORD_DEFAULT, options: IPasswordOpts = DEFAULT_OPTS): Promise<string> {
+  public static async hash(password: string, algo: Algorithms = PASSWORD_DEFAULT, options: IPasswordOpts = DEFAULT_OPTS): Promise<string> {
     // Make sure we are not using an insecure algorithm
     if(INSECURE_ALGORITHMS.includes(algo) && !options.allowInsecure) throw Error('Insecure hashing algorithm selected, aborting');
 
@@ -140,8 +114,9 @@ export class Password {
     let rounds = 2 ** cost;
     let result = input;
     for(let round = 0; round < rounds; round++) {
-      let buf = await crypto.subtle.digest(algo as DigestAlgorithm, new TextEncoder().encode(`${salt}${result}`));
-      result = [...new Uint8Array(buf)].map(x => x.toString(16).padStart(2, '0')).join('');
+      let h = new Hash(input, algo);
+      await h.digest();
+      result = await h.hex();
     }
 
     return result;
