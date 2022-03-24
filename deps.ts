2ad30c13c0d9517eb098e94b32dd3b8bd8044d00
Aroop Roelofs <me@finlaydag33k.nl>
2022-03-24 19:48:05 +0100

    Update websocket library

--- deps.ts
+++ deps.ts
@@ -3,7 +3,7 @@ export { crypto } from "https://deno.land/std@0.113.0/crypto/mod.ts";
 export { config as env } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 export { time as timets } from "https://denopkg.com/burhanahmeed/time.ts@v2.0.1/mod.ts";
 export { format as formatter } from "https://cdn.deno.land/std/versions/0.77.0/raw/datetime/mod.ts";
-export { WebSocketServer, WebSocketAcceptedClient } from "https://deno.land/x/websocket@v0.1.1/mod.ts";
+export { WebSocketServer, WebSocketAcceptedClient } from "https://deno.land/x/websocket@v0.1.3/mod.ts";
 export { engineFactory } from "https://deno.land/x/view_engine@v1.4.5/mod.ts";
 export { readerFromStreamReader } from "https://deno.land/std@0.126.0/io/mod.ts";
 export { Buffer } from "https://deno.land/std@0.87.0/node/buffer.ts";
