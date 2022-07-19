3e57f0019efb2eb462c7611c74683f9f3dda1040
Aroop Roelofs <me@finlaydag33k.nl>
2022-07-19 15:34:42 +0200

    More commenting and a check

--- communication/druid.ts
+++ communication/druid.ts
@@ -8,7 +8,13 @@ export class Druid {
   ) {
   }
 
-  public async create(): Promise<any> {
+  /**
+   * Create a new task in Apache Druid
+   *
+   * @returns Promise<Response>
+   */
+  public async create(): Promise<Response> {
+    if(!this.spec) throw Error('No task specification has been set!');
     return await fetch(`${this.host}/druid/indexer/v1/task`, {
       method: 'POST',
       body: this.spec,
