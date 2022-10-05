b79c92be74f68f5a7faefdeadac5a894da4050d3
Aroop Roelofs <me@finlaydag33k.nl>
2022-10-05 15:15:09 +0200

    Cleanup some of the code by removing pointless conversions from and to hex.

--- security/balloon.ts
+++ security/balloon.ts
@@ -6,7 +6,6 @@ export class Balloon {
   /**
    *
    * @param input The main string to hash
-   * @param algo The algorithm to be used
    * @param salt A user defined random value for security
    * @param space_cost The size of the buffer
    * @param time_cost Number of rounds to mix
@@ -15,8 +14,8 @@ export class Balloon {
    */
   public static async balloon(input: string, salt: string, space_cost: number = 16, time_cost: number = 20, delta: number = 3) {
     // Create our initial buffer
-    let buf = [
-      (await Balloon.doHash(0, input, salt)).hex(),
+    let buf: Hash[] = [
+      await Balloon.doHash(0, input, salt),
     ];
 
     // Set our counter to 1
@@ -28,13 +27,11 @@ export class Balloon {
     // Mix, mix, swirl mix.
     await Balloon.mix(buf, counter, delta, salt, space_cost, time_cost);
 
-    //const h = new Hash(`${salt}${input}`, Balloon.algo);
-    //await h.digest()
-    //return h.hex();
+    // Extract the last hash from our buffer and return it
     return Balloon.extract(buf);
   }
 
-  private static async doHash(...args: any[]) {
+  private static async doHash(...args: any[]): Promise<Hash> {
     const t = [];
 
     for(const arg of args) {
@@ -66,49 +63,41 @@ export class Balloon {
   }
 
 
-  private static async expand(buffer: string[], counter: number, space_cost: number): Promise<number> {
+  private static async expand(buffer: Hash[], counter: number, space_cost: number): Promise<number> {
     for(let s = 1; s < space_cost; s++) {
-      buffer.push((await Balloon.doHash(counter, Balloon.hexToBytes(buffer[s - 1]))).hex());
+      buffer.push(await Balloon.doHash(counter, buffer[s - 1].bytes()));
       counter++;
     }
     return counter;
   }
 
-  private static async mix(buffer: string[], counter: number, delta: number, salt: string, space_cost: number, time_cost: number) {
+  private static async mix(buffer: Hash[], counter: number, delta: number, salt: string, space_cost: number, time_cost: number): Promise<void> {
     for(let t = 0; t < time_cost; t++) {
       for(let s = 0; s < space_cost; s++) {
         if(typeof buffer.at(s-1) === 'undefined' || typeof buffer.at(s) === 'undefined') {
           throw Error('Buffer too short! (this is a bug!)');
         }
-        buffer[s] = (await Balloon.doHash(counter, Balloon.hexToBytes(buffer.at(s-1)!), Balloon.hexToBytes(buffer.at(s)!))).hex();
+        buffer[s] = await Balloon.doHash(counter, buffer.at(s-1)!.bytes(), buffer.at(s)!.bytes());
         counter++;
         for(let i = 0; i < delta; i++) {
-          const idx_block = (await Balloon.doHash(t, s, i)).hex();
-          const h = await Balloon.doHash(counter, salt, Balloon.hexToBytes(idx_block));
+          const idx_block = await Balloon.doHash(t, s, i);
+          const h = await Balloon.doHash(counter, salt, idx_block.bytes());
           const other = Number(Balloon.toNumber(h.bytes()) % BigInt(space_cost));
           counter++;
-          buffer[s] = (await Balloon.doHash(counter, Balloon.hexToBytes(buffer[s]), Balloon.hexToBytes(buffer[other]))).hex();
+          buffer[s] = await Balloon.doHash(counter, buffer[s].bytes(), buffer[other].bytes());
           counter++;
         }
       }
     }
   }
 
-  private static extract(buffer: string[]) {
-    return buffer.at(-1);
+  private static extract(buffer: Hash[]): string {
+    return buffer.at(-1)!.hex();
   }
 
-  private static hexToBytes(hex: string) {
-    const bytes = [];
-    for (let c = 0; c < hex.length; c += 2) {
-      bytes.push(parseInt(hex.substr(c, 2), 16));
-    }
-    return new Uint8Array(bytes);
-  }
-
-  private static toBytes(number: number) {
+  private static toBytes(number: number): number[] {
     // we want to represent the input as an 8-bytes array
-    const byteArray = [0, 0, 0, 0, 0, 0, 0, 0];
+    const byteArray: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
 
     for(let i = 0; i < byteArray.length; i++) {
       const byte = number & 0xff;
@@ -124,3 +113,6 @@ export class Balloon {
     return BigInt('0x' + byteArray.map((byte: any) => byte.toString(16).padStart(2, '0')).join(''));
   }
 }
+
+const res = await Balloon.balloon("hunter42", "examplesalt", 1024, 3, 3);
+console.log(res);
