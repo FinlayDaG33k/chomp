20d50c0c058b35ea5d41aa9553c0e73d6b8439c2
Aroop Roelofs <me@finlaydag33k.nl>
2023-07-16 15:15:52 +0200

    Add class for interfacting with Loki

---
+++ communication/loki.ts
@@ -0,0 +1,41 @@
+import { Logger } from "../logging/logger.ts";
+
+export interface LokiStream {
+  stream: any;
+  values: Array<Array<number|string>>;
+}
+
+export default class Loki {
+  /**
+   * @param host
+   * @param tenant Tenant ID to use for the log item. Can be ommitted if not using multi-tenant mode.
+   */
+  public constructor(
+    private readonly host: string = 'http://localhost:3100',
+    private readonly tenant: string = 'fake',
+  ) {
+  }
+
+  /**
+   * Send a log entry to the Grafana Loki database.
+   *
+   * @param stream
+   */
+  public async send(stream: LokiStream|LokiStream[]) {
+    try {
+      const resp = await fetch(`${this.host}/loki/api/v1/push`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Scope-OrgID': this.tenant,
+        },
+        body: JSON.stringify({
+          streams: Array.isArray(stream) ? stream : [stream],
+        }),
+      });
+      if(resp.ok === false) throw Error(`Response non-OK: ${resp.statusText}`)
+    } catch(e) {
+      Logger.error(`Could not add message to Loki: ${e.message}`, e.stack);
+    }
+  }
+}
