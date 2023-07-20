91ca284921844f8c5438d11be902c96b912a9292
Aroop Roelofs <me@finlaydag33k.nl>
2023-07-20 17:50:51 +0200

    Deprecate ucfirst and lcfirst in favour of the Inflector class

--- util/lcfirst.ts
+++ util/lcfirst.ts
@@ -1,3 +1,6 @@
+import { Logger } from "../logging/logger.ts";
+
 export function lcfirst(input: string): string {
+  Logger.debug('Usage of "util/lcfirst" is deprecated, please use "util/inflector#lcfirst" instead.');
   return input.charAt(0).toLowerCase() + input.slice(1);
 }
--- util/ucfirst.ts
+++ util/ucfirst.ts
@@ -1,3 +1,6 @@
+import { Logger } from "../logging/logger.ts";
+
 export function ucfirst(input: string): string {
+  Logger.debug('Usage of "util/ucfirst" is deprecated, please use "util/inflector#ucfirst" instead.');
   return input.charAt(0).toUpperCase() + input.slice(1);
 }
