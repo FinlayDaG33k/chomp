fe88e05650a9ec77d479436f969e77bddd05fca8
Aroop Roelofs <me@finlaydag33k.nl>
2023-07-28 19:27:02 +0200

    Fix a small naming conflict

--- webserver/routing/router.ts
+++ webserver/routing/router.ts
@@ -2,7 +2,7 @@ import { readerFromStreamReader } from "https://deno.land/std@0.126.0/io/mod.ts"
 import { pathToRegexp } from "../pathToRegexp.ts";
 import { Inflector } from "../../util/inflector.ts";
 import { Logger } from "../../logging/logger.ts";
-import { Request } from "../http/request.ts";
+import { Request as ChompRequest } from "../http/request.ts";
 
 interface Route {
   path: string;
@@ -84,7 +84,7 @@ export class Router {
     // Run our controller
     try {
       // Instantiate the controller
-      const controller = new Router._cache[args.route.controller][`${args.route.controller}Controller`](new Request(args));
+      const controller = new Router._cache[args.route.controller][`${args.route.controller}Controller`](new ChompRequest(args));
 
       // Execute our action
       await controller[args.route.action]();
