d2924ca1c955dd23f82e522080d6799f8ae4d1d2
Aroop Roelofs <me@finlaydag33k.nl>
2023-07-28 17:45:48 +0200

    Add utility function to raise errors

--- util/mod.ts
+++ util/mod.ts
@@ -3,6 +3,7 @@ export * from "./folder-exists.ts";
 export * from "./inflector.ts";
 export * from "./is-ts.ts";
 export * from "./lcfirst.ts";
+export * from "./raise.ts";
 export * from "./time-string.ts";
 export * from "./tokenizer.ts";
 export * from "./ucfirst.ts";
---
+++ util/raise.ts
@@ -0,0 +1,8 @@
+/**
+ * Utility function that literally just throws an error
+ * 
+ * @param err
+ */
+export function raise(err: string): never {
+  throw new Error(err);
+}
