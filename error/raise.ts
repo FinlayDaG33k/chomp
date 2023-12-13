b9cd05be347a768c6d677fc03566385131086aab
Aroop Roelofs <me@finlaydag33k.nl>
2023-12-14 00:20:04 +0100

    Update raise to quickly be able to make custom error names

--- error/raise.ts
+++ error/raise.ts
@@ -1,8 +1,17 @@
 /**
- * Utility function that literally just throws an error
+ * Utility function that literally just throws an error.
+ * TODO: Allow passing custom error classes for better extendability.
  *
  * @param err
+ * @param type
  */
-export function raise(err: string): never {
-  throw new Error(err);
+export function raise(err: string, type: string|'Error' = 'Error'): never {
+  // Check if we want to throw a regular error
+  if(type === "Error") throw new Error(err);
+  
+  // Build a custom error and throw it
+  if(type.slice(-5).toLowerCase() !== "error") type = `${type}Error`;
+  const e = new Error(err);
+  e.name = type;
+  throw e;
 }
