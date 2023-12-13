b2aed3ef58c3a6f93a0c019001c060b2ab925be7
Aroop Roelofs <me@finlaydag33k.nl>
2023-12-14 00:24:15 +0100

    Allow passing custom error classes

--- error/raise.ts
+++ error/raise.ts
@@ -1,13 +1,15 @@
 /**
  * Utility function that literally just throws an error.
- * TODO: Allow passing custom error classes for better extendability.
  *
  * @param err
  * @param type
  */
-export function raise(err: string, type: string|'Error' = 'Error'): never {
+export function raise(err: string, type: string|Function|'Error' = 'Error'): never {
   // Check if we want to throw a regular error
   if(type === "Error") throw new Error(err);
+  
+  // Check if we want to throw a specific error class
+  if(typeof type === "function") throw new type(err);
   
   // Build a custom error and throw it
   if(type.slice(-5).toLowerCase() !== "error") type = `${type}Error`;
