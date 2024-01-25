058e2931634f0ca4693f735da4e36568abe77749
Aroop Roelofs <me@finlaydag33k.nl>
2024-01-25 17:17:33 +0100

    Comments and allow creation of directories

--- filesystem/folder.ts
+++ filesystem/folder.ts
@@ -1,9 +1,14 @@
+import { Logger } from "../logging/logger.ts";
+
 export class Folder {
   public constructor(
     private readonly path: string
   ) {
   }
-  
+
+  /**
+   * Check whether the folder exists at the path.
+   */
   public async exists(): Promise<boolean> {
     try {
       const target = await Deno.stat(this.path);
@@ -12,5 +17,20 @@ export class Folder {
       if(e instanceof Deno.errors.NotFound) return false;
       throw e;
     }
+  }
+
+  /**
+   * Create the directory if it does not exist yet.
+   * 
+   * @param options Options with which to create the directory
+   */
+  public async create(options?: Deno.MkdirOptions): Promise<void> {
+    try {
+      if(await this.exists()) throw new Error('The specified folder already exists!');
+    } catch(e) {
+      Logger.warning(e.message);
+    }
+
+    await Deno.mkdir(this.path, options);
   }
 }
