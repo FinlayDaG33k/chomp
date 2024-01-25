ca18b4c7cb313e41d80d4b96bdce2c4a1737122c
Aroop Roelofs <me@finlaydag33k.nl>
2024-01-25 09:40:43 +0100

    Remove "raise" from default package

--- mod.ts
+++ mod.ts
@@ -5,7 +5,6 @@
 export { Cache } from "./core/cache.ts";
 export { Configure } from "./core/configure.ts";
 export { Logger } from "./logging/logger.ts";
-export { raise } from "./error/raise.ts";
 export { File } from "./filesystem/file.ts";
 export { Folder } from "./filesystem/folder.ts";
 export { CheckSource } from "./utility/check-source.ts";
