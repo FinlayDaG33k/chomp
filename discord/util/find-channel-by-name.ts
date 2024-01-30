962f04e428b49973d672c557210a026066f8c756
Aroop Roelofs <me@finlaydag33k.nl>
2024-01-30 13:41:39 +0100

    Remove any

--- discord/util/find-channel-by-name.ts
+++ discord/util/find-channel-by-name.ts
@@ -1,4 +1,4 @@
-import { Discord } from "../mod.ts";
+import { Discord, Channel } from "../mod.ts";
 import { Logger } from "../../logging/logger.ts";
 
 /**
@@ -10,14 +10,14 @@ import { Logger } from "../../logging/logger.ts";
  * @param guild
  * @param name
  */
-export async function findChannelByName(guild: BigInt, name: string): Promise<unknown> {
+export async function findChannelByName(guild: BigInt, name: string): Promise<Channel|null> {
   const channels = await Discord.getBot().helpers.getChannels(guild);
   if(!channels) {
     Logger.error(`Could not obtain channels for guild! (this is a bug!)`);
     return null;
   }
 
-  const channel = channels.find((channel: any) => channel.name === name.toLowerCase());
+  const channel = channels.find((channel: Channel) => channel.name === name.toLowerCase());
   if(!channel) {
     Logger.error(`No channel with name "#${name}" could be found! (did you set it up yet?)`);
     return null;
