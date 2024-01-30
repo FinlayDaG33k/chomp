2910e9d17f071c4fe1fb5ae52a8cd7390709925d
Aroop Roelofs <me@finlaydag33k.nl>
2024-01-30 17:37:24 +0100

    Update "raise"

--- error/raise.ts
+++ error/raise.ts
@@ -1,19 +1,25 @@
 /**
- * Utility function that literally just throws an error.
- *
+ * Utility function that throws an error.
+ * Band-aid for JS not supporting throwing in null-coalescing.
+ *  
  * @param err
  * @param type
  */
-export function raise(err: string, type: string|Function|'Error' = 'Error'): never {
+// deno-lint-ignore no-explicit-any ban-types -- TODO
+export function raise<TError extends Error>(err: string, type: string|(new (...args: any[]) => TError)|'Error' = 'Error'){
   // Check if we want to throw a regular error
   if(type === "Error") throw new Error(err);
   
-  // Check if we want to throw a specific error class
-  if(typeof type === "function") throw new type(err);
+  // Check if we want to throw a specific name only
+  if(typeof type === "string") {
+    if(type.slice(-5).toLowerCase() !== "error") type = `${type}Error`;
+    const e = new Error(err);
+    e.name = type as string;
+    throw e;
+  }
   
-  // Build a custom error and throw it
-  if(type.slice(-5).toLowerCase() !== "error") type = `${type}Error`;
-  const e = new Error(err);
-  e.name = type;
-  throw e;
+  // Check if we want to throw a specific error class
+  if(type.prototype instanceof Error) throw new type(err);
 }
+
+//raise('Test', 'BlahError');
