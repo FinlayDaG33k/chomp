1f35ac3c10be371af0ffef46de351f5aedb4328b
Aroop Roelofs <me@finlaydag33k.nl>
2024-02-02 01:29:43 +0100

    Update raise function and add tests (Closes #11)

--- error/raise.ts
+++ error/raise.ts
@@ -5,21 +5,23 @@
  * @param err
  * @param type
  */
-// deno-lint-ignore no-explicit-any -- TODO
-export function raise<TError extends Error>(err: string, type: string|(new (...args: any[]) => TError)|'Error' = 'Error'){
-  // Check if we want to throw a regular error
-  if(type === "Error") throw new Error(err);
-  
-  // Check if we want to throw a specific name only
-  if(typeof type === "string") {
-    if(type.slice(-5).toLowerCase() !== "error") type = `${type}Error`;
-    const e = new Error(err);
-    e.name = type as string;
+export function raise<CustomError extends Error>(err: string, type: string|(new (err: string) => CustomError)|'Error' = 'Error'){
+  // Check if we want to throw a specific class
+  if(typeof type === 'function' && type.prototype instanceof Error) {
+    const e = new type(err);
+    Error.captureStackTrace(e, raise);
+    e.name = type.name;
     throw e;
   }
   
-  // Check if we want to throw a specific error class
-  if(type.prototype instanceof Error) throw new type(err);
+  // Initialize regular error
+  // Then add our stacktrace
+  const e = new Error(err);
+  Error.captureStackTrace(e, raise);
+  
+  // Check if we want to change the name
+  if(type !== "Error") e.name = (type as string).slice(-5).toLowerCase() !== "error" ? `${type}Error`: type as string;
+  
+  // Throw the error
+  throw e;
 }
-
-//raise('Test', 'BlahError');
