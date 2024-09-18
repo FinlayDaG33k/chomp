86d40cc206d1178e0968e64d5830fd0d62cb9f1e
Aroop Roelofs <me@finlaydag33k.nl>
2024-09-18 18:08:30 +0200

    Fix small test error

--- core/logger.ts
+++ core/logger.ts
@@ -54,9 +54,9 @@ export class Logger {
    * @param level {LogLevels}
    * @param handler {any}
    */
-  // @ts-ignore TODO: Figure out how to replace Function with something more sane
-  // deno-lint-ignore ban-types -- TODO
   public static setHandler(level: LogLevels, handler: Function): void {
+    // @ts-ignore TODO: Figure out how to replace Function with something more sane
+    // deno-lint-ignore ban-types -- TODO
     Logger._handlers[level] = handler;
   }
 
