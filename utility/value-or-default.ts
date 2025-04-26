a2b5a562ff87bdb3b519b36fea3c996c61c16b40
Aroop Roelofs <me@finlaydag33k.nl>
2025-04-26 20:09:46 +0200

    Significantly simplify the statement

--- utility/value-or-default.ts
+++ utility/value-or-default.ts
@@ -5,12 +5,5 @@
  * @param defaultValue
  */
 export function valueOrDefault(input: unknown, defaultValue: unknown): unknown {
-  // Check if undefined
-  if(input === undefined) return defaultValue;
-
-  // Check if null
-  if(input === null) return defaultValue;
-
-  // We have a value just return the input
-  return input;
+  return input ?? defaultValue;
 }
