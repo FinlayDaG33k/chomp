a876829ef1a963e17796f8971b19999fc8f48cea
Aroop Roelofs <me@finlaydag33k.nl>
2025-07-14 10:49:42 +0200

    Fix small bug in is-null

--- validation/rules/is-null.ts
+++ validation/rules/is-null.ts
@@ -2,5 +2,5 @@ import {ValidationCallbackResponse, ValidationOptions} from "../validator.ts";
 
 export function isNull(input: any, options: ValidationOptions): ValidationCallbackResponse {
   if(input === null) return [undefined];
-  return [undefined];
+  return [options.message];
 }
