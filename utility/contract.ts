3b6760a62d1e0d87e20232b880675e7faa32c0d6
Aroop Roelofs <me@finlaydag33k.nl>
2025-11-11 01:33:53 +0100

    Update contracts to assert

--- utility/contract.ts
+++ utility/contract.ts
@@ -6,7 +6,7 @@ import { nameOf } from "./name-of.ts";
  * Class to more easily throw errors while creating (among others) constructors.
  * Allows code to be more concise and users to more easily read what it does.
  *
- * It was based on the .NET 6 feature of the same name.
+ * Its idea was based on the .NET 6 feature of the same name.
  */
 export class Contract {
   /**
@@ -35,23 +35,14 @@ export class Contract {
    * import { Contract } from "https://deno.land/x/chomp/utility/contract.ts";
    *
    * const myArgument = "blabla";
-   * Contract.requireNotNull(myArgument, "myArgument");
-   * ```
-   *
-   * @example Using the {@linkcode nameOf} utility
-   * ```ts
-   * import { Contract } from "https://deno.land/x/chomp/utility/contract.ts";
-   * import { nameOf } from "https://deno.land/x/chomp/utility/name-of.ts";
-   *
-   * const myArgument = "blabla";
-   * Contract.requireNotNull(myArgument, nameOf({ myArgument }));
+   * Contract.requireNotNull(myArgument);
    * ```
    *
    * @param argument
-   * @param argumentName
+   * @param message
    */
-  public static requireNotNull(argument: unknown, argumentName: string): void|never {
-    if (argument === null) raise(`${argumentName} may not be null`, "ContractArgumentNull");
+  public static requireNotNull<T>(argument: T, message?: string): asserts argument is Exclude<T, null> {
+    if (argument === null) raise(message ? message : `Contract failed, argument ("${argument}") was null`, "ContractArgumentNull");
   }
 
   /**
@@ -62,28 +53,39 @@ export class Contract {
    * import { Contract } from "https://deno.land/x/chomp/utility/contract.ts";
    *
    * const myArgument = "blabla";
-   * Contract.requireNotUndefined(myArgument, "myArgument");
-   * ```
-   *
-   * @example Using the {@linkcode nameOf} utility
-   * ```ts
-   * import { Contract } from "https://deno.land/x/chomp/utility/contract.ts";
-   * import { nameOf } from "https://deno.land/x/chomp/utility/name-of.ts";
-   *
-   * const myArgument = "blabla";
-   * Contract.requireNotUndefined(myArgument, nameOf({ myArgument }));
+   * Contract.requireNotUndefined(myArgument);
    * ```
    *
    * @param argument
-   * @param argumentName
+   * @param message
    */
-  public static requireNotUndefined(argument: unknown, argumentName: string): void|never {
-    if(argument === undefined) raise(`${argumentName} may not be undefined`, "ContractArgumentUndefined");
+  public static requireNotUndefined<T>(argument: T, message?: string): asserts argument is Exclude<T, undefined> {
+    if(argument === undefined) raise(message ? message : `Contract failed, argument ("${argument}") was undefined`, "ContractArgumentUndefined");
+  }
+
+  /**
+   * Require the input to not be nullish.
+   *
+   * Internally acts as a proxy for {@linkcode Contract.requireNotUndefined} and {@linkcode Contract.requireNotNull}.
+   *
+   * @example Basic Usage
+   * ```ts
+   * import { Contract } from "https://deno.land/x/chomp/utility/contract.ts";
+   *
+   * const myArgument = "blabla";
+   * Contract.requireNotNullish(myArgument);
+   * ```
+   *
+   * @param argument
+   * @param message
+   */
+  public static requireNotNullish<T>(argument: T, message?: string): asserts argument is Exclude<Exclude<T, null>, undefined> {
+    Contract.requireNotUndefined(argument, message);
+    Contract.requireNotNull(argument, message);
   }
 
   /**
    * Require the input argument to not be empty
-   *
    *
    * @param argument
    */
