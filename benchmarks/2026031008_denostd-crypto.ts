a3c21dd81ab922083aed4243bcda83582384568b
Aroop Roelofs <me@finlaydag33k.nl>
2026-07-31 09:23:50 +0200

    Fix benchmark to show proper results

--- benchmarks/2026031008_denostd-crypto.ts
+++ benchmarks/2026031008_denostd-crypto.ts
@@ -2,24 +2,28 @@ import { Random } from "../src/security/random.ts";
 import { crypto as cryptoOld } from "https://deno.land/std@0.113.0/crypto/mod.ts";
 import { crypto as cryptoNew } from "jsr:@std/crypto@1.1.0/crypto";
 
-// Generate dataset
-const dataset: Uint8Array[] = [];
-for(let i = 0; i < 10_000; i++) {
+Deno.bench("Crypto 0.113.0", {n: 100_000 }, async (b) => {
+  // Prepare bytes
   const bytes = Random.bytes(128);
   const string = bytes.toHex();
+  const encoded = new TextEncoder().encode(string);
 
-  dataset.push(new TextEncoder().encode(string));
-}
+  // Benchmark
+  b.start();
+  await cryptoOld.subtle.digest("BLAKE3", encoded);
+  b.end();
 
-Deno.bench("Crypto 0.113.0", async () => {
-  for(const entry of dataset) {
-    await cryptoOld.subtle.digest("BLAKE3", entry);
-  }
 });
 
-Deno.bench("Crypto 1.1.0", async () => {
-  for(const entry of dataset) {
-    await cryptoNew.subtle.digest("BLAKE3", entry);
-  }
+Deno.bench("Crypto 1.1.0", {n: 10_000 }, async (b) => {
+  // Prepare bytes
+  const bytes = Random.bytes(128);
+  const string = bytes.toHex();
+  const encoded = new TextEncoder().encode(string);
+
+  // Benchmark
+  b.start();
+  await cryptoNew.subtle.digest("BLAKE3", encoded);
+  b.end();
 });
 
